0377488587a6b977a8c0da1a479dc85baeaafd6c
moises.oliveira <moises.oliveira@spread.com.br>
2022-01-06 20:37:01 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/moisessilva10/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.moisessilva10.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moisessilva10.cursomc.domain.Categoria;
+import com.moisessilva10.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElse(null);
+	}
+
+}
