45fc804662ac6d42cd162d4bad0190bcce8da9c6
moises.oliveira <moises.oliveira@spread.com.br>
2022-01-07 15:05:44 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/moisessilva10/cursomc/services/CategoriaService.java
+++ src/main/java/com/moisessilva10/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.moisessilva10.cursomc.services;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +15,17 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
+//	public Categoria buscar(Integer id) {
+//		
+//		Optional<Categoria> obj = repo.findById(id);
+//		
+//		return obj.orElse(null);
+//	}
+//	
 	public Categoria buscar(Integer id) {
-		
 		Optional<Categoria> obj = repo.findById(id);
-		
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
+		}
 
 }
